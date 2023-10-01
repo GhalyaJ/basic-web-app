3abeb2f2879e4e4d29b5a16b1aeed400899103e0
gjohar@andrew.cmu.edu <yourgitemail@example.com>
2023-10-01 12:49:01 +0300

    editied andrew question

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -5,7 +5,7 @@ export default function QueryProcessor(query: string): string {
       "English poet, playwright, and actor, widely regarded as the greatest " +
       "writer in the English language and the world's pre-eminent dramatist."
     );
-  } else if (query.toLowerCase().includes("andrew id")) {
+  } else if (query.toLowerCase().includes("andrew")) {
     return "My Andrew ID is gjohar";
   }
 
